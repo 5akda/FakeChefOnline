b02c35a7a58872b676df19393c281fefcf157f45
Sakda Suwantheerangkoon <Sakda.Suwantheerangkoon@krungsri.com>
2023-06-04 20:18:12 +0700

    Init Project

---
+++ app/src/main/java/iam5akda/fakechef/online/MainActivity.kt
@@ -0,0 +1,46 @@
+package iam5akda.fakechef.online
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import iam5akda.fakechef.online.ui.theme.FakeChefOnlineTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FakeChefOnlineTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FakeChefOnlineTheme {
+        Greeting("Android")
+    }
+}
