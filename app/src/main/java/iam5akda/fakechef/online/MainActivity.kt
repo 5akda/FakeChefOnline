52c191e9797e06346c33facbc2d60408760083fa
Sakda Suwantheerangkoon <Sakda.Suwantheerangkoon@krungsri.com>
2023-06-05 00:25:52 +0700

    [Splash Screen] Implement Splash & Launcher Icon

--- app/src/main/java/iam5akda/fakechef/online/MainActivity.kt
+++ app/src/main/java/iam5akda/fakechef/online/MainActivity.kt
@@ -2,45 +2,31 @@ package iam5akda.fakechef.online
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import iam5akda.fakechef.online.ui.theme.FakeChefOnlineTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().also {
+            it.setKeepOnScreenCondition { true }
+        }
         super.onCreate(savedInstanceState)
-        setContent {
-            FakeChefOnlineTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        navigateToHome()
+    }
+
+    private fun navigateToHome() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                delay(1500L)
+                finish()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FakeChefOnlineTheme {
-        Greeting("Android")
     }
 }
