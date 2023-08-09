3593c4ff77b0d8971cd2e5c3a931ddb4a8b224d0
Sakda Suwantheerangkoon <sakdakluy@gmail.com>
2023-08-10 00:10:53 +0700

    [Feature Home] Random Food Pic

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val CORE_KTX = "1.10.1"
     const val LIFECYCLE_RUNTIME_KTS = "2.6.1"
-    const val DAGGER_HILT = "2.44.2"
+    const val DAGGER_HILT = "2.47"
     const val SPLASH_SCREEN = "1.0.1"
     const val COMPOSE_BOM = "2023.03.00"
     const val ACTIVITY_COMPOSE = "1.7.2"
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -55,7 +55,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
 
     androidTestImplementation(platform("androidx.compose:compose-bom:${Versions.COMPOSE_BOM}"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
