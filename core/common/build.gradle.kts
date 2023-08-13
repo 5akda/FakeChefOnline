0ee0585604fe9e8f59948ee94b8cf6e063846c97
Sakda Suwantheerangkoon <sakdakluy@gmail.com>
2023-08-14 00:27:28 +0700

    [Game Prepare] Fix Gradle

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -15,7 +15,11 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
--- core/design/build.gradle.kts
+++ core/design/build.gradle.kts
@@ -13,7 +13,11 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
